1aa5ac8d1cf212db0e56b95f3d6b8d03c0918fd0
Pratikshadange2005 <pratiksha.dange@gwmail.gwu.edu>
2024-10-13 16:07:53 -0400

    Remove 'Event' suffix from event types and commit the staging layer

---
+++ transform/models/staging/stg_gharchive.sql
@@ -0,0 +1,9 @@
+select
+  REPLACE(type, 'Event', '') as event_type,
+  actor.login as user,
+  repo.id as repo_id,
+  repo.name as repo_name,
+  created_at as event_date
+from {{ source("gharchive", "src_gharchive") }}
+
+
