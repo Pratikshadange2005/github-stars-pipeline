3488166bf460e97594b042613ca4588ccc7edcd5
Pratikshadange2005 <pratiksha.dange@gwmail.gwu.edu>
2024-10-24 01:13:16 -0400

    Updated  SQL query to avoid repetition and improve readability

--- transform/models/marts/repo_pushes_daily.sql
+++ transform/models/marts/repo_pushes_daily.sql
@@ -3,7 +3,7 @@ select distinct
   repo_name,
   date_trunc('day', event_date) as date_day,
   count(*) as push_count,
-  sum(count(*)) over (partition by repo_id order by date_trunc('day', event_date)) as cumul_push_count
+  sum(push_count) over (partition by repo_id order by date_day) as cumul_push_count
 from {{ ref("stg_gharchive") }}
 where event_type = 'Push'
-group by repo_id, repo_name, date_day
+group by 1, 2, 3
