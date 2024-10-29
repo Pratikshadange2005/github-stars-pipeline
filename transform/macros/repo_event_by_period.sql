44fa4ecfb3e77f67efff16e1801ba53516d80365
Pratikshadange2005 <pratiksha.dange@gwmail.gwu.edu>
2024-10-29 14:45:20 -0400

    Updated requested changes

---
+++ transform/macros/repo_event_by_period.sql
@@ -0,0 +1,11 @@
+{% macro repo_event_by_period(event_type, period) -%}
+select distinct
+  repo_id,
+  repo_name,
+  date_trunc('{{ period }}', event_date) as date_{{ period }},
+  count(*) as {{ event_type }}_count,
+  sum({{ event_type }}_count) over (partition by repo_id order by date_{{ period }}) as cumul_{{ event_type }}_count
+from {{ ref("stg_gharchive") }}
+where event_type = '{{ event_type}}'
+group by 1, 2, 3
+{%- endmacro %}
